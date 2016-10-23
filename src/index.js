8f77553233bc0963c1295ef3cd96d38f695658af
Igor Ribeiro <igor.ribeiro.plus@gmail.com>
2016-10-23 00:03:54 -0200

    cleanup

--- src/index.js
+++ src/index.js
@@ -4,17 +4,5 @@ import App from './App';
 
 import './global.css';
 
-function getRoute() {
-  return window.location.hash.substring(0);
-}
+render(<App />, document.getElementById('root'));
 
-console.log(getRoute());
-
-function renderApp() {
-  render(
-    <App />,
-    document.getElementById('root')
-  );
-}
-
-renderApp();
