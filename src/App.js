53b803181a5a0fb39ba2e74af612f9e4537d2cff
Igor Ribeiro <igor.ribeiro.plus@gmail.com>
2016-10-22 22:58:58 -0200

    implement listing and adding bets

--- src/App.js
+++ src/App.js
@@ -2,13 +2,71 @@ import React, { Component } from 'react';
 
 import './App.css';
 
+let nextBetId = 1;
+
+const BetSchema = {
+  id: null,
+  description: '',
+  participants: 0,
+};
+
 class App extends Component {
+  state = {
+    bets: [
+      { id: 0, description: 'Horário de chegada do Matheus', participants: 1 },
+    ],
+    newBetDescription: '',
+  }
+
+  constructor() {
+    super();
+
+    this.handleNewBetChange = this.handleNewBetChange.bind(this);
+    this.handleNewBetSubmit = this.handleNewBetSubmit.bind(this);
+  }
+
+  handleNewBetChange(event) {
+    this.setState({
+      newBetDescription: event.target.value,
+    });
+  }
+
+  handleNewBetSubmit(event) {
+    event.preventDefault();
+
+    const { bets, newBetDescription } = this.state;
+
+    bets.push({
+      ...BetSchema,
+      id: nextBetId,
+      description: newBetDescription,
+    });
+
+    this.setState({ bets, newBetDescription: '' }, () => nextBetId++);
+  }
+
   render() {
+    const { bets, newBetDescription } = this.state;
+
     return (
-      <div className="app-container">
-        <div className="app">
-          <h1>Initial</h1>
-        </div>
+      <div className="app">
+        <h2>Bolões</h2>
+
+        <form onSubmit={ this.handleNewBetSubmit } action="">
+          <input value={ newBetDescription } onChange={ this.handleNewBetChange } />
+          <button type="submit">Adicionar</button>
+        </form>
+
+        <hr />
+
+        <ul>
+          { bets.map(bet => (
+            <li key={ bet.id }>
+              { bet.description }
+              <div>Participantes: { bet.participants }</div>
+            </li>
+          )) }
+        </ul>
       </div>
     );
   }
